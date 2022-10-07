3f6433af1384e3166922342a09eeb53a478614ea
Sadiah Shaikh <69952394+sadiah7@users.noreply.github.com>
2022-10-07 20:14:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 
 app.config['MYSQL_HOST'] = 'bll6rxbmrx78s6i1xgr2-mysql.services.clever-cloud.com'
+app.config['MYSQL_DATABASE_URI'] = 'mysql://unzcpyamk9tfvpfx:ug5QtkyFF113DmviwdcU@bll6rxbmrx78s6i1xgr2-mysql.services.clever-cloud.com:3306/bll6rxbmrx78s6i1xgr2'
 app.config['MYSQL_USER'] = 'unzcpyamk9tfvpfx'
 app.config['MYSQL_PASSWORD'] = 'ug5QtkyFF113DmviwdcU'
 app.config['MYSQL_DB'] = 'bll6rxbmrx78s6i1xgr2'
